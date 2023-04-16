b25a2f61ad9269be29ad3208e7928a2c0f5a7a0c
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-04-16 16:57:36 -0400

    masks

--- rocket-man/src/screens/Home.tsx
+++ rocket-man/src/screens/Home.tsx
@@ -2,7 +2,12 @@ export default function Home() {
   return (
     <>
       <button className="gradient-button">
-        <div className="gradient-button-wrapper"></div>ok
+        <div className="gradient-button-topleft-mask"></div>
+        <div className="gradient-button-topright-mask"></div>
+        <div className="gradient-button-bottomleft-mask"></div>
+        <div className="gradient-button-bottomright-mask"></div>
+        <div className="gradient-button-wrapper"></div>
+        <span className="gradient-button-text-span">ok</span>
       </button>
     </>
   );
