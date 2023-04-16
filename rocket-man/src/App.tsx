f76e44174debd75d03a81743fa0ab2894bba5f6e
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-04-16 01:46:54 -0400

    router

--- rocket-man/src/App.tsx
+++ rocket-man/src/App.tsx
@@ -2,11 +2,26 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Scene from "./Scene";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import { useRef } from "react";
 
 function App() {
   return (
     <div className="App">
-      <Scene />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/scene"
+            element={<Scene />}
+          />
+        </Routes>
+      </BrowserRouter>
+      {/*<Scene />*/}
     </div>
   );
 }
