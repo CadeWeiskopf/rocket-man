218c49dcad380eb5f4d728e90151bfeea9ea3dfe
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-04-16 16:08:42 -0400

    making border little more round

--- rocket-man/src/screens/Home.tsx
+++ rocket-man/src/screens/Home.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <button className="gradient-button">Button</button>
+      <button className="gradient-button">
+        <div className="gradient-button-wrapper"></div>ok
+      </button>
     </>
   );
 }
