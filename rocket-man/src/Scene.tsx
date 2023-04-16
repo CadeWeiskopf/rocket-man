8cf0fd4400bab39688255093821c1c668046568a
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-04-16 01:07:00 -0400

    init

---
+++ rocket-man/src/Scene.tsx
@@ -0,0 +1,72 @@
+import React, { useRef, useEffect } from "react";
+import * as THREE from "three";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+import { EXRLoader } from "three/examples/jsm/loaders/EXRLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+
+export default function Scene() {
+  const mountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // create a new scene
+    const scene = new THREE.Scene();
+
+    // create a new camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.y = 0;
+    camera.position.z = 5;
+
+    // create a new renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mountRef.current?.appendChild(renderer.domElement);
+
+    // create a directional light and add it to the scene
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(0, 1, 1).normalize();
+    scene.add(light);
+
+    const mtlLoader = new MTLLoader();
+    mtlLoader.load("rocket.mtl", (materials) => {
+      materials.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(materials);
+      objLoader.load("rocket.obj", (object) => {
+        scene.add(object);
+      });
+    });
+
+    // load the EXR environment map
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load("ge.glb", (gltf) => {
+      const cube = gltf.scene.children[0];
+      scene.add(gltf.scene);
+    });
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+    animate();
+
+    const handleResize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <div ref={mountRef} />;
+}
