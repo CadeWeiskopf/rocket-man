91f1bb4cd64ea3b07995f415d584fdea51cfed80
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-04-16 17:47:51 -0400

    painstaking idea for corners.

--- rocket-man/src/screens/Home.tsx
+++ rocket-man/src/screens/Home.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <>
       <button className="gradient-button">
-        <div className="gradient-button-topleft-mask"></div>
+        <div className="gradient-button-topleft-mask">
+          <div className="gradient-button-topleft-mask2"></div>
+        </div>
         <div className="gradient-button-topright-mask"></div>
         <div className="gradient-button-bottomleft-mask"></div>
         <div className="gradient-button-bottomright-mask"></div>
