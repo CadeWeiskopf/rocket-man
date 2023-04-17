9a7f81b2ee6f83fba16e3864cfe393b462202f91
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-04-16 21:33:23 -0400

    corners finishing

--- rocket-man/src/screens/Home.tsx
+++ rocket-man/src/screens/Home.tsx
@@ -5,7 +5,15 @@ export default function Home() {
         <div className="gradient-button-topleft-mask">
           <div className="gradient-button-topleft-mask2"></div>
         </div>
+        <div className="gradient-button-topright-mask">
+          <div className="gradient-button-topright-mask2"></div>
+        </div>
+        <div className="gradient-button-bottomright-mask">
+          <div className="gradient-button-bottomright-mask2"></div>
+        </div>
         <div className="gradient-button-wrapper-topleft"></div>
+        <div className="gradient-button-wrapper-topright"></div>
+        <div className="gradient-button-wrapper-bottomright"></div>
         <span className="gradient-button-text-span">ok</span>
       </button>
     </>
